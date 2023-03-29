015a1ece93c250821eb6e9846547524d8bd2aa4a
mariaguaza123 <43611721+mariaguaza123@users.noreply.github.com>
2023-03-29 13:01:01 -0500

    Update index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const server = require('./services/server');
-const puerto = 8080;
+const express = require("express");
+const port = process.env.PORT || 3001;
 
 server.listen(puerto, () =>{
 console.log(`Servidor listo escuchando en el puerto ${puerto}`);
